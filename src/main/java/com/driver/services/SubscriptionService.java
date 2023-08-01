52057e6362419dace38a11435c7b44a01756126a
Harit <haritshandilya.beas@gmail.com>
2023-08-01 23:23:01 +0530

    Try this

--- src/main/java/com/driver/services/SubscriptionService.java
+++ src/main/java/com/driver/services/SubscriptionService.java
@@ -7,11 +7,13 @@ import com.driver.model.SubscriptionType;
 import com.driver.model.User;
 import com.driver.repository.SubscriptionRepository;
 import com.driver.repository.UserRepository;
+import jdk.internal.loader.AbstractClassLoaderValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SubscriptionService {
@@ -25,8 +27,21 @@ public class SubscriptionService {
     public Integer buySubscription(SubscriptionEntryDto subscriptionEntryDto){
 
         //Save The subscription Object into the Db and return the total Amount that user has to pay
-
-        return null;
+        Integer amount=null;
+        Optional<User> optionalUser = userRepository.findById(subscriptionEntryDto.getUserId());
+        if(!optionalUser.isPresent()) return null;
+        User user = optionalUser.get();
+        if(subscriptionEntryDto.getSubscriptionType()==SubscriptionType.BASIC){
+            amount = 500 + subscriptionEntryDto.getNoOfScreensRequired()*200;
+        }else if(subscriptionEntryDto.getSubscriptionType()==SubscriptionType.PRO){
+            amount = 800 + subscriptionEntryDto.getNoOfScreensRequired()*250;
+        }else if(subscriptionEntryDto.getSubscriptionType()==SubscriptionType.ELITE){
+            amount = 1000 + subscriptionEntryDto.getNoOfScreensRequired()*350;
+        }
+        Subscription sb = new Subscription(subscriptionEntryDto.getSubscriptionType(),subscriptionEntryDto.getNoOfScreensRequired(),new Date(), amount);
+        sb.setUser(user);
+        subscriptionRepository.save(sb);
+        return amount;
     }
 
     public Integer upgradeSubscription(Integer userId)throws Exception{
